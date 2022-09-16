264eb4b4141999d3077b558099757ed83baf15d8
Jeremy Levy <jje.levy@gmail.com>
2022-09-16 18:00:39 +0200

    bump cli version

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -41,7 +41,7 @@ From there, the most common workflow is:
 
 	TraverseChildren: true,
 
-	Version: "v0.0.14",
+	Version: "v0.0.15",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
